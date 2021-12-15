fe6653ed6e8b4bb12013de9b9cf635dd156e1ee1
Manuel <manuel@marginleft.at>
2021-12-15 20:01:18 +0100

    font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      body: ['"Cormorant Garamond"', "serif"]
+    },
     extend: {},
   },
   variants: {
